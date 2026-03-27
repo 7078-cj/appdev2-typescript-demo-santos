507c833fc1aa801369a396f6970d402d579f59a1
7078-cj <7078ceejay@gmail.com>
2026-03-27 14:29:08 +0800

    Done playing and updating the 06-alternative-to-enums.ts

--- 06-alternative-to-enum.ts
+++ 06-alternative-to-enum.ts
@@ -2,10 +2,21 @@
     // Literal Types (Alternative to Enum)
     let userRole: 'admin' | 'guess' | 'teacher' | 'student' = 'admin'
 
-    userRole = 'guess'
+    // userRole = 'superadmin'
+    // Type '"superadmin"' is not assignable to type '"admin" | "guess" | "teacher" | "student"'.ts(2322)
+    // let userRole: "admin" | "guess" | "teacher" | "student"
+    // nag error to dahil hindi assigned ang superadmin sa options ng types
+
+    userRole = 'student'
+
     // anotherUserRole = 'superadmin'
 
     function access(role: 'admin' | 'guess' | 'teacher' | 'student') {
         // ...
     }
+
+    // access('superadmin')
+    // Argument of type '"superadmin"' is not assignable to parameter of type '"admin" | "guess" | "teacher" | "student"'.ts(2345)
+    access('student')
+
 })
---
+++ compiled/06-alternative-to-enum.js
@@ -0,0 +1,17 @@
+"use strict";
+(() => {
+    // Literal Types (Alternative to Enum)
+    let userRole = 'admin';
+    // userRole = 'superadmin'
+    // Type '"superadmin"' is not assignable to type '"admin" | "guess" | "teacher" | "student"'.ts(2322)
+    // let userRole: "admin" | "guess" | "teacher" | "student"
+    // nag error to dahil hindi assigned ang superadmin sa options ng types
+    userRole = 'student';
+    // anotherUserRole = 'superadmin'
+    function access(role) {
+        // ...
+    }
+    // access('superadmin')
+    // Argument of type '"superadmin"' is not assignable to parameter of type '"admin" | "guess" | "teacher" | "student"'.ts(2345)
+    access('student');
+});
