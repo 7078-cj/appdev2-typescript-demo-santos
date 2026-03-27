acbce5de613393d62a6c64e352ff5ff597ad805a
7078-cj <7078ceejay@gmail.com>
2026-03-27 14:02:31 +0800

    Done playing and updating the 03-flexible-types.ts

--- 03-flexibile-types.ts
+++ 03-flexibile-types.ts
@@ -1,10 +1,10 @@
-let age: any = 31
-
+let age: any = 20 // dito number ang data type 
+// any data type is nag a adapt sia depende kung ano ung declared data type nung value nia
 // ...
 
-age = '31'
-age = true
-age = {}
-age = []
+age = '20' // type: string
+age = true // type: boolean
+age = {}// type: object
+age = []// type: object
 
 console.log(typeof age)
