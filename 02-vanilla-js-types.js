2565c3113907a44908bae42cba0d50f797ad61b6
7078-cj <7078ceejay@gmail.com>
2026-03-27 13:50:46 +0800

    Done playing and updating the 02-vanilla-js-types.js

--- 02-vanilla-js-types.js
+++ 02-vanilla-js-types.js
@@ -1,3 +1,5 @@
-let userName1 = "Elmer";
+let userName1 = "Ceejay";
+//dahil sa pag set natin sa value ng variable ay string ang magiging data type ng variable is string
 
 console.log(typeof userName1)
+//output: string
