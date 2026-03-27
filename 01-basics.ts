2c262fc34873f381a8036e0918f4e39bc4a0a481
7078-cj <7078ceejay@gmail.com>
2026-03-27 13:44:46 +0800

    Done playing and updating the 01-basic.ts file

--- 01-basics.ts
+++ 01-basics.ts
@@ -1,11 +1,12 @@
 // console.log("Hello World!")
 
-let userName: string = "Elmer";
-let userAge = 31;
-
+let userName: string = "Ceejay";
+let userAge = 20;
+// let userAge: number
 // ...
 
-userAge = "31"
+// userAge = "31" nag e error to kasi dapat number ang laman ng variable hindi string
+userAge = 31
 
 console.log(`Username - ${userName}`)
 console.log(`Age - ${userAge}`)
@@ -15,6 +16,6 @@ function add(a: number, b = 5) {
 }
 
 add(10)
-// add('10')
+// add('10') mag e error to dahil numer ang expected data type sa parameter ng function
 add(10, 6)
-// add(10, '6')
+// add(10, '6') mag e error parin to dahil sa pag declare ng parameter is number kaya automatic na data type ng parmeter b ay number
---
+++ compiled/01-basics.js
@@ -0,0 +1,17 @@
+"use strict";
+// console.log("Hello World!")
+let userName = "Ceejay";
+let userAge = 20;
+// let userAge: number
+// ...
+// userAge = "31" nag e error to kasi dapat number ang laman ng variable hindi string
+userAge = 31;
+console.log(`Username - ${userName}`);
+console.log(`Age - ${userAge}`);
+function add(a, b = 5) {
+    return a + b;
+}
+add(10);
+// add('10') mag e error to dahil numer ang expected data type sa parameter ng function
+add(10, 6);
+// add(10, '6') mag e error parin to dahil sa pag declare ng parameter is number kaya automatic na data type ng parmeter b ay number
