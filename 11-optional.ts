cfabc4b735e3a488c52508633860f67766a7c8f4
7078-cj <7078ceejay@gmail.com>
2026-03-27 14:55:00 +0800

    Done playing and updating the 11-optional.ts

--- 11-optional.ts
+++ 11-optional.ts
@@ -3,8 +3,8 @@ function generateError(msg?: string) {
     throw new Error(msg)
 }
 
-generateError("An error occured") // Automatic Semicolon Insertion pitfall - add ; to explicitly tell that this is a function
-// generateError()
+generateError("An error occured"); // Automatic Semicolon Insertion pitfall - add ; to explicitly tell that this is a function
+generateError(); // optional dahil sa msg?  
 
 
 (() => {
@@ -12,21 +12,34 @@ generateError("An error occured") // Automatic Semicolon Insertion pitfall - add
     type User = {
         name: string;
         age: number;
-        role?: 'admin' | 'guess'
+        role?: 'admin' | 'guess' // ito ay optional
     }
 
     let user: User = {
-        name: 'Elmer',
-        age: 31
+        name: 'Ceejay',
+        age: 20,
+        role: 'admin'
+    }
+
+    let user2: User = {
+        name: 'Santos',
+        age: 20,
     }
 
     user.name
     user.age
+    user.role
+    
+    user2.name
+    user2.age
+    user2.role // either 'admin' | 'guess' or undefined
 })
 
 // Nullish Coalescing `??` operator 
 let input = ''
 const didProvideInput = input ?? false;
+
+console.log(input ?? false)
 
 // try it on browser console log to see results
 // update it `??` to `||`
